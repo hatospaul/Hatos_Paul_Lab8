1b05fe1c476194e44406a8ba0e3e4bb28ab01044
Hatos Paul <hatospaul@gmail.com>
2021-11-24 12:28:11 +0200

    Add project files.

---
+++ Data/Hatos_Paul_Lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Hatos_Paul_Lab8.Models;
+
+namespace Hatos_Paul_Lab8.Data
+{
+    public class Hatos_Paul_Lab8Context : DbContext
+    {
+        public Hatos_Paul_Lab8Context (DbContextOptions<Hatos_Paul_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Hatos_Paul_Lab8.Models.Book> Book { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Hatos_Paul_Lab8.Data;
+using Hatos_Paul_Lab8.Models;
+
+namespace Hatos_Paul_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Hatos_Paul_Lab8.Data.Hatos_Paul_Lab8Context _context;
+
+        public IndexModel(Hatos_Paul_Lab8.Data.Hatos_Paul_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.ToListAsync();
+        }
+    }
+}
