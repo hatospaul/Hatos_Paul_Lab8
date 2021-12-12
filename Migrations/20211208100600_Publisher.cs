5a9c466cda7afe99496d2c1e6be14b6446c77e0e
Hatos Paul <hatospaul@gmail.com>
2021-12-12 20:59:44 +0200

    Lab 8 complet

---
+++ Migrations/20211208100600_Publisher.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hatos_Paul_Lab8.Migrations
+{
+    public partial class Publisher : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
